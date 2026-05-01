2b8e3ca777a30218939f577f9398a3b1c088cf19
Joshua Chen <jzc0289@auburn.edu>
2026-04-30 20:19:16 -0500

    feat: create build build script

--- main.c
+++ main.c
@@ -1,9 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
-	printf("argc= %d\n", argc);
-	for (int i = 0; i < argc; i++)
-		printf("argc[%d] = %s\n", i, argv[i]);
+	# expected values
+	int tokenSize = 16;
+	int tokenCount = 128;
 
-	printf("argv[argc] = %p\n", (void*)argv[argc]);
-}
+	char expr[tokenSize * tokenCount];
+	int ind = 0;
+
+	for (int i = 1; i < argc; i++) {
+		int argSize = strlen(argv[i]);
+
+		for (int j = 0; j < argSize; j++) {
+			char currChar = argv[i][j];
+
+			if (isspace(currChar)) {
+				continue;
+			}
+
+			printf("%c", argv[i][j]);
+			ind++;
+		}
+	}
+	printf("\n");
+
+	# tokenize
+
+	# list of token as strings
+	char tokens[tokenCount][tokenSize]
+
+	int tokInd = 0;
+	int chrInd = 0;
+	bool isNeg = True;
+
+	for (int i = 0; i < ind; i++) {
+		if () {
+			
+		}
+		else if () {
+			# next token
+		}
+	}
+} 
